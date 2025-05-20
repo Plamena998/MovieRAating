7b8373443e21350c5194694e50685cb0c395a3f8
Plamena998 <plami_d@mail.bg>
2025-05-20 09:38:26 +0300

    update

---
+++ Data Context/MovieDbContext.cs
@@ -0,0 +1,12 @@
+﻿
+using Fluent.Infrastructure.FluentModel;
+using MovieRating2000.Models;
+using System.Data.Entity;
+
+namespace Data_Context
+{
+    public class MovieDbContext : ApplicationDbContext
+    {
+        DbSet<Cast> Casts
+    }
+}
